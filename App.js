fa9ab5b831a6a7504d7de0a24104abf827a94fe9
brunommbruno <brunommbruno@gmail.com>
2021-02-03 16:27:17 +0000

    main screen navbar

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import * as Device from "expo-device";
+
+//main screens
+import NewsScreen from "./screens/NewsScreen";
+import RacesScreen from "./screens/RacesScreen";
+import ChampionshipsScreen from "./screens/ChampionshipsScreen";
+import FavouritesScreen from "./screens/FavouritesScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          //customises each tab
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "News") {
+              iconName = focused ? "newspaper" : "newspaper-outline";
+            } else if (route.name === "Races") {
+              iconName = focused ? "bicycle" : "bicycle-outline";
+            } else if (route.name === "Championships") {
+              iconName = focused ? "trophy" : "trophy-outline";
+            } else if (route.name === "Favourites") {
+              iconName = focused ? "star" : "star-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "settings" : "settings-outline";
+            }
+            return <Ionicons name={iconName} size={35} color={color} />;
+          },
+          title: "",
+        })}
+        tabBarOptions={{
+          activeTintColor: "#F2771A",
+          inactiveTintColor: "#4A4242",
+          style: {
+            //accounting for extra bottom space for swipe gestures on no home button iphones
+            height:
+              Platform.OS === "android"
+                ? 60
+                : Device.modelName === "iPhone 7"
+                ? 60
+                : 100,
+            padding: Platform.OS === "android" ? 15 : 10,
+          },
+        }}
+      >
+        <Tab.Screen name="News" component={NewsScreen} />
+        <Tab.Screen name="Races" component={RacesScreen} />
+        <Tab.Screen name="Championships" component={ChampionshipsScreen} />
+        <Tab.Screen name="Favourites" component={FavouritesScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
