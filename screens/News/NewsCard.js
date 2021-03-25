dd1f364fa5afc51e21f2426a40e088266ebbd86a
brunommbruno <brunommbruno@gmail.com>
2021-03-25 14:41:05 +0000

    added main/sub newscard

--- screens/News/NewsCard.js
+++ screens/News/NewsCard.js
@@ -7,21 +7,28 @@ import {
   SafeAreaView,
   ImageBackground,
   Linking,
+  Image,
 } from "react-native";
 import { TouchableHighlight } from "react-native-gesture-handler";
 
-const image = {
-  uri:
-    "https://www.rallyraidnetwork.com/rally/images/news/2019/09-september/baja-portalegre-8-9-w.jpg",
-};
+export default function NewsCard({ image_url, link, title, type }) {
+  const image = {
+    uri: image_url,
+  };
 
-export default function NewsCard() {
-  return (
-    <TouchableHighlight onPress={() => Linking.openURL("http://google.com")}>
+  return type === "main" ? (
+    <TouchableHighlight onPress={() => Linking.openURL(link)}>
       <View style={styles.newsCard}>
         <ImageBackground source={image} style={styles.image}>
-          <Text style={styles.cardTitle}>Lorem Ipsum dolor sit amet</Text>
+          <Text style={styles.cardTitle}>{title}</Text>
         </ImageBackground>
+      </View>
+    </TouchableHighlight>
+  ) : (
+    <TouchableHighlight onPress={() => Linking.openURL(link)}>
+      <View style={styles.subNewsCard}>
+        <Image source={image} style={styles.subImage}></Image>
+        <Text style={styles.subCardTitle}>{title}</Text>
       </View>
     </TouchableHighlight>
   );
@@ -36,7 +43,7 @@ const styles = StyleSheet.create({
   newsCard: {
     height: 200,
     width: "100%",
-    paddingBottom: 5,
+    marginBottom: 30,
   },
   cardTitle: {
     bottom: 0,
@@ -44,8 +51,23 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(22, 22, 22, 0.38)",
     width: "100%",
     padding: 2,
-    fontSize: 20,
+    fontSize: 30,
     color: "white",
     fontStyle: "italic",
+  },
+  subNewsCard: {
+    height: 80,
+    width: "100%",
+    marginBottom: 10,
+    flexDirection: "row",
+  },
+  subCardTitle: {
+    width: "60%",
+    fontSize: 15,
+    color: "white",
+    paddingLeft: 10,
+  },
+  subImage: {
+    width: "40%",
   },
 });
--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -23,10 +23,30 @@ export default function News() {
 
       {selectedMenu === "news" ? (
         <ScrollView>
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
+          <NewsCard
+            title="Lorem Ipsum dolor sit amet"
+            link="https://www.youtube.com/embed/I8eUZUCKDzc"
+            image_url="https://matraxlubricants.com/wp-content/uploads/2020/11/1.jpg"
+            type="main"
+          />
+          <NewsCard
+            title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+            link="https://www.youtube.com/embed/I8eUZUCKDzc"
+            image_url="https://www.bajaportalegre500.com/ResourcesUser/2020/Imagens/Fotos/Quads.jpg"
+            type="sub"
+          />
+          <NewsCard
+            title="Duis aute irure dolor in reprehenderit in voluptate velit "
+            link="https://www.youtube.com/embed/I8eUZUCKDzc"
+            image_url="https://www.rallyraidnetwork.com/rally/images/news/2019/10-october/map-top-21-10-w.jpg"
+            type="sub"
+          />
+          <NewsCard
+            title="Excepteur sint occaecat cupidatat noner proident, sunt in culpa qui officia "
+            link="https://www.youtube.com/embed/I8eUZUCKDzc"
+            image_url="https://eurol.com/app/uploads/2020/11/ten-brinke-viert-overwinning.jpg"
+            type="sub"
+          />
         </ScrollView>
       ) : (
         <ScrollView>
