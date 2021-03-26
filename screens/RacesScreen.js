d619bfcf995e47c1d2e6f3a926150c380d3ae8fa
brunommbruno <brunommbruno@gmail.com>
2021-03-26 01:04:25 +0000

    added axios and races screen GET method for race event instances

--- screens/RacesScreen.js
+++ screens/RacesScreen.js
@@ -1,11 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button } from "react-native";
+import axios from "../axios";
 
 export default function RacesScreen() {
+  const [races, setRaces] = useState();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (loaded === false) {
+      axios.get(`/race-event-instances`).then(({ data }) => {
+        setRaces(data.data);
+        setLoaded(true);
+      });
+    }
+  });
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Races</Text>
+      {races ? (
+        races.map((race) => (
+          <View style={styles.raceItem} key={race.race_event_instance_id}>
+            <Text style={styles.raceItemText}>
+              {race.championship_event_instance_id}, {race.race_event_id},{" "}
+              {race.race_event_instance_id}, {race.status}, {race.date_from}/
+              {race.date_to}
+            </Text>
+          </View>
+        ))
+      ) : (
+        <Text style={styles.loading}>Loading...</Text>
+      )}
     </SafeAreaView>
   );
 }
@@ -13,7 +38,22 @@ export default function RacesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+  },
+  loading: {
+    paddingTop: 100,
+    textAlign: "center",
+    color: "white",
+  },
+  raceItem: {
+    marginBottom: 10,
+    width: "100%",
+    padding: 20,
+    borderWidth: 1,
+    borderColor: "white",
     justifyContent: "center",
-    alignItems: "center",
+  },
+  raceItemText: {
+    color: "white",
   },
 });
