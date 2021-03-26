a9ca5e83224e7e63c99fc5b72e0fde19672b2b3c
brunommbruno <brunommbruno@gmail.com>
2021-03-26 16:01:44 +0000

    added padding container to main newsscreen

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -20,48 +20,49 @@ export default function News() {
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="black" style="light" />
       <NewsNav setMenu={setMenu} selectedMenu={selectedMenu} />
-
-      {selectedMenu === "news" ? (
-        <ScrollView>
-          <NewsCard
-            title="Lorem Ipsum dolor sit amet"
-            link="https://www.youtube.com/embed/I8eUZUCKDzc"
-            image_url="https://matraxlubricants.com/wp-content/uploads/2020/11/1.jpg"
-            type="main"
-          />
-          <NewsCard
-            title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
-            link="https://www.youtube.com/embed/I8eUZUCKDzc"
-            image_url="https://www.bajaportalegre500.com/ResourcesUser/2020/Imagens/Fotos/Quads.jpg"
-            type="sub"
-          />
-          <NewsCard
-            title="Duis aute irure dolor in reprehenderit in voluptate velit "
-            link="https://www.youtube.com/embed/I8eUZUCKDzc"
-            image_url="https://www.rallyraidnetwork.com/rally/images/news/2019/10-october/map-top-21-10-w.jpg"
-            type="sub"
-          />
-          <NewsCard
-            title="Excepteur sint occaecat cupidatat noner proident, sunt in culpa qui officia "
-            link="https://www.youtube.com/embed/I8eUZUCKDzc"
-            image_url="https://eurol.com/app/uploads/2020/11/ten-brinke-viert-overwinning.jpg"
-            type="sub"
-          />
-        </ScrollView>
-      ) : (
-        <ScrollView>
-          <MediaCard
-            title={
-              "FORA DE ESTRADA T2 EP05 (TT Internacional e Portugueses no Dakar 2021)"
-            }
-            media_url={"https://www.youtube.com/embed/C9hI57_-QkI"}
-          />
-          <MediaCard
-            title={"FORA DE ESTRADA T2 EP03 (Dakar 2021)"}
-            media_url={"https://www.youtube.com/embed/I8eUZUCKDzc"}
-          />
-        </ScrollView>
-      )}
+      <View style={styles.main}>
+        {selectedMenu === "news" ? (
+          <ScrollView>
+            <NewsCard
+              title="Lorem Ipsum dolor sit amet"
+              link="https://www.youtube.com/embed/I8eUZUCKDzc"
+              image_url="https://matraxlubricants.com/wp-content/uploads/2020/11/1.jpg"
+              type="main"
+            />
+            <NewsCard
+              title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+              link="https://www.youtube.com/embed/I8eUZUCKDzc"
+              image_url="https://www.bajaportalegre500.com/ResourcesUser/2020/Imagens/Fotos/Quads.jpg"
+              type="sub"
+            />
+            <NewsCard
+              title="Duis aute irure dolor in reprehenderit in voluptate velit "
+              link="https://www.youtube.com/embed/I8eUZUCKDzc"
+              image_url="https://www.rallyraidnetwork.com/rally/images/news/2019/10-october/map-top-21-10-w.jpg"
+              type="sub"
+            />
+            <NewsCard
+              title="Excepteur sint occaecat cupidatat noner proident, sunt in culpa qui officia "
+              link="https://www.youtube.com/embed/I8eUZUCKDzc"
+              image_url="https://eurol.com/app/uploads/2020/11/ten-brinke-viert-overwinning.jpg"
+              type="sub"
+            />
+          </ScrollView>
+        ) : (
+          <ScrollView>
+            <MediaCard
+              title={
+                "FORA DE ESTRADA T2 EP05 (TT Internacional e Portugueses no Dakar 2021)"
+              }
+              media_url={"https://www.youtube.com/embed/C9hI57_-QkI"}
+            />
+            <MediaCard
+              title={"FORA DE ESTRADA T2 EP03 (Dakar 2021)"}
+              media_url={"https://www.youtube.com/embed/I8eUZUCKDzc"}
+            />
+          </ScrollView>
+        )}
+      </View>
     </SafeAreaView>
   );
 }
@@ -70,5 +71,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === "android" ? 20 : 0,
+  },
+  main: {
+    padding: 15,
   },
 });
